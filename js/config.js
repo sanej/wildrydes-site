aba012a1aef8db65a5b1a06c4160d91ddd078c9c
sanej <88908918+sanej@users.noreply.github.com>
2024-05-17 15:42:07 -0700

    Update userPoolId and userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_G44v6OBIG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'rdmv4ahsd0os9p36pces8q1a8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://master.d3svl5xb1r42x5.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
